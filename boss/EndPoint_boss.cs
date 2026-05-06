0bb056c6fa551d133779da0aba2f1dd5124739b4
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-06 21:20:29 +0900

    Remove unused Update method from EndPoint_boss
    Removed unused Update method and cleaned up code.

--- boss/EndPoint_boss.cs
+++ boss/EndPoint_boss.cs
@@ -5,15 +5,9 @@ using UnityEngine;
 public class EndPoint_boss : MonoBehaviour
 {
     public BoxCollider2D End_collider;
-    // Start is called before the first frame update
+  
     void Start()
     {
         End_collider = GetComponent<BoxCollider2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
