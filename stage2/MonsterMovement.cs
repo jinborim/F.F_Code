d713311f157d0ef08d72b583d2163178ae545027
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-06 22:51:47 +0900

    Update MonsterMovement.cs

--- stage2/MonsterMovement.cs
+++ stage2/MonsterMovement.cs
@@ -5,137 +5,74 @@ using UnityEngine.SceneManagement;
 
 public class MonsterMovement : MonoBehaviour
 {
-    Rigidbody2D monsterRd;
-    public int nextMove;
+
+    private Rigidbody2D monsterRd;
+    public int nextMove; 
     public float speed = 2f;
-    public int M_health;
+    public int M_healtㅗ;
 
-    public bool is_endpoint;
-    public HP_Manager hp_manger;
+   public bool is_endpoint; 
+    public HP_Manager hp_manger; 
 
-    public Monster monster_;
-    public CharacterMovement character;
-    public Boss_Movement Boss;
+    public Monster monster_; 
+    public CharacterMovement character; 
+    public Boss_Movement Boss; 
 
-
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void Awake()
     {
-        //캐릭터는 기본적으로 isTrigger가 활성화되어있지 않기때문에 이 함수를 사용한다.
-        if (collision.gameObject.CompareTag("Character"))
-        {
-            if (character.is_Beat == false)
-            {
-                if (hp_manger == null)
-                {
-                    hp_manger = GameObject.FindObjectOfType<HP_Manager>();
-                }
-                hp_manger.Damaged(monster_.damage);
-                //StartCoroutine(character.OnBeatTime());
+        monsterRd = GetComponent<Rigidbody2D>();
+    }
 
-            }
-            
-
-        }
+    private void Start()
+    {
+        is_endpoint = false;
         
+        hp_manger = GameObject.FindObjectOfType<HP_Manager>();
+        character = GameObject.FindObjectOfType<CharacterMovement>();
+        Boss = GameObject.FindObjectOfType<Boss_Movement>();
+
+        if (monster_ != null)
+        {
+            M_health = monster_.M_health;
+        }
+    }
+
+     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        HandleDamage(collision.gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Character"))
-        {
-            if (character.is_Beat == false)
-            {
-                if (hp_manger == null)
-                {
-                    hp_manger = GameObject.FindObjectOfType<HP_Manager>();
-                }
-                hp_manger.Damaged(monster_.damage);
-                //StartCoroutine(character.OnBeatTime());
-
-            }
-
-
-        }
-        
+        HandleDamage(collision.gameObject);
     }
 
- 
+    private void HandleDamage(GameObject target)
+    {
+        if (!target.CompareTag("Character"))
+            return;
+
+        if (character == null || character.is_Beat)
+            return;
+
+        if (hp_manager == null || monster_ == null)
+            return;
+
+        hp_manager.Damaged(monster_.damage);
+    }
+
     public void health_manager(int damage)
     {
-        this.M_health -= damage;
-        if (this.M_health <= 0)
+        M_health -= damage;
+
+        if (M_health <= 0)
         {
-            Destroy(gameObject);
             if (Boss != null)
             {
                 Boss.Rest_count -= 1;
             }
 
+            Destroy(gameObject);
         }
     }
-   
-
-    private void Start()
-    {
-        is_endpoint = false;
-        hp_manger = GameObject.FindObjectOfType<HP_Manager>();
-        M_health = monster_.M_health;
-        character = GameObject.FindObjectOfType<CharacterMovement>();
-        Boss = GameObject.FindObjectOfType<Boss_Movement>();
-    }
-
-    private void Awake()
-    {
-        monsterRd = GetComponent<Rigidbody2D>();
-        //Invoke("Think", 5);//초기화 함수 안에 넣어서 실행될 때 마다 nextMove변수가 초기화됨
-    }
-    // Start is called before the first frame update
-
-    /*
-    void FixedUpdate()
-    {
-        if (Inventory.invectoryActivated == false)
-        {
-            //인벤토리가 열려있을 때는 몬스터도 멈추도록
-            MonsterMove();
-        }
-        
-    
-    }
-
-    void MonsterMove()
-    {
-        monsterRd.velocity = new Vector2(nextMove, monsterRd.velocity.y);
-
-
-        //platform check(맵 앞이 낭떨어지면 뒤돌기 위해 지형 탐색)
-
-        //자신의 한 칸 앞 지형 탐색
-        Vector2 frontVec = new Vector2(monsterRd.position.x + nextMove * 0.4f, monsterRd.position.y);
-
-        Debug.DrawRay(frontVec, Vector3.down*10, new Color(0, 1, 0));
-
-        RaycastHit2D raycast = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
-
-        if (raycast.collider == null||is_endpoint==true)
-        {
-            //Debug.Log("이 앞은 낭떠러지");
-            nextMove = nextMove * (-1);//낭떠러지니깐 방향을 반대로 바꿈
-            CancelInvoke();//Think를 잠시 멈춘 후 재실행
-            Invoke("Think", 5);
-        }
-    }
-
-
-    void Think()
-    {
-        nextMove = Random.Range(-1, 2);
-        float time = Random.Range(2f, 5f);
-        
-        //Think()재귀함수: 딜레이를 쓰지않으면 CPU과부화 되므로 재귀함수를 쓸 때는 항상 주의
-        //Think()를 직접 호출하는대신 Invoke()사용
-        Invoke("Think", time);//매개변수로 받은 함수를 5초의 딜레이를 부여하여 재실행
-    }
-    */
 }
