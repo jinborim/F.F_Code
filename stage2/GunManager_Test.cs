15d0b83409c62db8e776c0e323770c220fc0970e
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-06 22:50:33 +0900

    Update GunManager_Test.cs

--- stage2/GunManager_Test.cs
+++ stage2/GunManager_Test.cs
@@ -48,3 +48,45 @@ public class GunManager_Test : MonoBehaviour
     }
 
 }
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+  [SerializeField] private float speed;
+
+    private bool isMovingLeft = true;
+
+    void Start()
+    {
+        speed = Random.Range(2f, 6f);
+    }
+
+    void Update()
+    {
+        if (Inventory.invectoryActivated) return;
+
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    private void TurnAround()
+    {
+        isMovingLeft = !isMovingLeft;
+
+        float yRotation = isMovingLeft ? 0f : 180f;
+        transform.eulerAngles = new Vector3(0f, yRotation, 0f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("endpoint")) return;
+
+        TurnAround();
+    }
+}
