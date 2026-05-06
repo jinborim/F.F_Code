a173a9742d44ee6c2533d52ae1f5899f8a537f6a
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-06 21:26:32 +0900

    Update SetStartPoint.cs

--- boss/SetStartPoint.cs
+++ boss/SetStartPoint.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 public class SetStartPoint : MonoBehaviour
 {
     private GameObject thePlayer;
+    
     public float Xpoint;
     public float Ypoint;
     public Vector2 startpoint;
 
-    // Start is called before the first frame update
     void Start()
     {
-        startpoint = new Vector2(Xpoint, Ypoint);
-        if (thePlayer == null)
-        {
-            thePlayer = GameObject.Find("Character");
-        }
-            
-        thePlayer.transform.position = startpoint;
+       startpoint = new Vector2(Xpoint, Ypoint);
 
-        if(Dialogue_Maintain.Instance.GetComponentInChildren<Dialog_Test>()&& Dialogue_Maintain.Instance.GetComponentInChildren<Select_base>()!= null)
+        if (thePlayer == null)
+            thePlayer = GameObject.Find("Character");
+
+        if (thePlayer != null)
+            thePlayer.transform.position = startpoint;
+
+        var dialogMaintain = Dialogue_Maintain.Instance;
+
+        if (dialogMaintain != null)
         {
-            Dialogue_Maintain.Instance.GetComponentInChildren<Dialog_Test>().gameObject.SetActive(false);
-            Dialogue_Maintain.Instance.GetComponentInChildren<Select_base>().gameObject.SetActive(false);
+            Dialog_Test dialog = dialogMaintain.GetComponentInChildren<Dialog_Test>();
+            Select_base select = dialogMaintain.GetComponentInChildren<Select_base>();
+
+            if (dialog != null)
+                dialog.gameObject.SetActive(false);
+
+            if (select != null)
+                select.gameObject.SetActive(false);
         }
-    
     }
 }
