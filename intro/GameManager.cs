d4ab96106e50209106f0d4ecdfa7879faf12021b
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-06 21:32:16 +0900

    Refactor GameManager for cleaner code and logic

--- intro/GameManager.cs
+++ intro/GameManager.cs
@@ -7,73 +7,61 @@ public class GameManager : MonoBehaviour
 {
     public GameObject talkPanel;
     public Text gtext;
-    public bool isAction; //대화창 활성화 상태 
 
-    //씬 2의 몬스터 삭제
+    public bool isAction;
+
     public GameObject monster;
     public enemyDead enemyControl;
     public PlayerMovement player;
 
-    // Start is called before the first frame update
     void Start()
     {
         isAction = false;
+
         talkPanel = GameObject.Find("talkPanel");
-        gtext = talkPanel.gameObject.GetComponentInChildren<Text>();
-        enemyControl = GameObject.FindObjectOfType<enemyDead>();
-        monster = enemyControl.transform.gameObject;
-        player = GameObject.FindObjectOfType<PlayerMovement>();
 
-        talkPanel.gameObject.SetActive(false);
-    }
+        if (talkPanel != null)
+            gtext = talkPanel.GetComponentInChildren<Text>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        enemyControl = FindObjectOfType<enemyDead>();
+
+        if (enemyControl != null)
+            monster = enemyControl.gameObject;
+
+        player = FindObjectOfType<PlayerMovement>();
+
+        if (talkPanel != null)
+            talkPanel.SetActive(false);
     }
 
     public void Action()
     {
-        if (isAction) // 실행중인데 또 액션을 실행한경우 -> 이미 true인경우
-        {
-            isAction = false; //false로 변경
+        isAction = !isAction;
 
-        }
-        else //실행중 -> 대화창 띄우기 
-        {
-            isAction = true;
-        }
-
-        talkPanel.SetActive(isAction); //대화창 활성화 상태에 따라 대화창 활성화 변경
+        if (talkPanel != null)
+            talkPanel.SetActive(isAction);
     }
 
     public void Scene22(bool scene2)
     {
-        if (scene2 == true)
+        if (scene2 && monster != null)
         {
-            //bulletReach = 3;
             Destroy(monster, 0.5f);
-
-            //Invoke("OnInvoke", 30.0f);
         }
-        Invoke("Call0", 1f);
+
+        Invoke(nameof(Call0), 1f);
     }
-    public void Call0()
+
+    private void Call0()
     {
-        StartCoroutine(player.Scene33(true));
+        if (player != null)
+            StartCoroutine(player.Scene33(true));
     }
 
     public void panel(bool is_true)
     {
-        if (is_true == true)
-        {
-            talkPanel.gameObject.SetActive(true);
-        }
-        else if (is_true == false)
-        {
-            talkPanel.gameObject.SetActive(false);
-        }
-    }
+        if (talkPanel == null) return;
 
+        talkPanel.SetActive(is_true);
+    }
 }
