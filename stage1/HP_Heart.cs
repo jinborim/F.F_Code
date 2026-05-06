40fce0ec1e3ca63a3e896ca6438312ea22b7dc91
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-06 21:59:18 +0900

    Update HP_Heart.cs

--- stage1/HP_Heart.cs
+++ stage1/HP_Heart.cs
@@ -4,34 +4,39 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class HP_Heart : MonoBehaviour
-{
-/**
-* 유저의 생존과 직결되는 체력(Heart UI)을 제어
-**/
-    public GameObject hp_heart;
-    public Image hp_Heart;
+{ 
+    [SerializeField] private Image hpHeart;
 
-    public float filled_amount;
-    public int Heart_Health = 100;
+    public int maxHealth = 100;
+    public int currentHealth;
 
     private void Awake()
     {
-        // 씬 전환이나 재시작 시 발생할 수 있는 데이터 오류를 방지하기 위해 초기화
-        Heart_Health = 100;
+        currentHealth = maxHealth;
     }
 
     void Start()
     {
-        //현재 스크립트가 포함된 게임 오브젝트를 변수에 할당
-        hp_heart = this.gameObject;
-        // hp_heart 오브젝트에 붙어 있는 Image 컴포넌트를 가져와서 hp_Heart 변수에 저장한다
-        hp_Heart = hp_heart.GetComponent<Image>();
+        if (hpHeart == null)
+            hpHeart = GetComponent<Image>();
+
+        UpdateUI();
     }
 
-
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int damage)
     {
-        
+        currentHealth = Mathf.Clamp(currentHealth - damage, 0, maxHealth);
+        UpdateUI();
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        hpHeart.fillAmount = (float)currentHealth / maxHealth;
     }
 }
