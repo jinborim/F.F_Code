0881f96221e0458823d85a9482ba372aca49c05d
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-06 21:37:44 +0900

    Update scene loading in skipButton.cs

--- intro/skipButton.cs
+++ intro/skipButton.cs
@@ -12,7 +12,6 @@ public class skipButton : MonoBehaviour, IPointerClickHandler
     {
         if (eventData.button == PointerEventData.InputButton.Left)
         {
-            //SceneManager.LoadScene(stage.sceneNow);
             LoadSceneManager.LoadScene("stage1");
         }
     }
