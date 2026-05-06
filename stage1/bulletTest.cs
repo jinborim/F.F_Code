df3620102725bcea6a1fdf9e37520c2fbd631c48
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-06 22:46:07 +0900

    Update bulletTest.cs

--- stage1/bulletTest.cs
+++ stage1/bulletTest.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 캐릭터에게 할당해서 총알이 캐릭터로부터 나오게끔 했다. 나중에 총으로 옮겨서 적용해보려고 함.
-
 public class bulletTest : MonoBehaviour
 {
     GameObject Character;
     GameObject FIREGUN;
-    //private GunInventory guninventory;
-    
     
     public GameObject bulletPrefab;
-    //public string Active_Bullet;
     public GunType_selected gun;
 
     public SoundEffect_Manager soundEffect;
@@ -23,10 +18,14 @@ public class bulletTest : MonoBehaviour
     {
         Character = GameObject.Find("Character");
         soundEffect = GameObject.FindObjectOfType<SoundEffect_Manager>();
-        //캐릭터라는 이름의 오브젝트로 부터 가져올 것을 저장하는 변수
+    }
+    
+    void Update()
+    {
+        bullet_func();
         
     }
-
+    
     void bullet_func()
     {
         
@@ -39,8 +38,6 @@ public class bulletTest : MonoBehaviour
 
                 Rigidbody2D bb = bullet.GetComponent<Rigidbody2D>();
 
-
-                //총알의 방향 결정
                 if (Character.GetComponent<CharacterMovement>().left == true)
                 {
                     bb.AddForce(Vector2.left * bulletSpeed, ForceMode2D.Impulse);
@@ -50,15 +47,8 @@ public class bulletTest : MonoBehaviour
                     bb.AddForce(Vector2.right * bulletSpeed, ForceMode2D.Impulse);
                 }
             }
-            
-
         }
-
     }
-
-
-    
-
 
     public void bullet_changer_test(GunType_selected _gun)
     {
@@ -72,13 +62,5 @@ public class bulletTest : MonoBehaviour
         {
             bulletPrefab = null;
         }
-    }
-   
-
-
-    void Update()
-    {
-        bullet_func();
-        
     }
 }
