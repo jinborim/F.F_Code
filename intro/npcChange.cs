2013e9cd8b21b582c20223bc335e06aa3c74f920
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-06 21:36:17 +0900

    Update npcChange.cs

--- intro/npcChange.cs
+++ intro/npcChange.cs
@@ -5,28 +5,23 @@ using UnityEngine.UI;
 
 public class npcChange : MonoBehaviour
 {
-    public Image img;
-    public Sprite after_img;
+     private Image img;
+    private RectTransform rect;
 
-    public RectTransform rect;
+    [SerializeField] private Sprite after_img;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         img = GetComponent<Image>();
         rect = GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void ChangeImage()
     {
-        //img.sprite = after_img;
-        rect.eulerAngles= new Vector3(0,180,0);
+        if (img != null && after_img != null)
+            img.sprite = after_img;
+
+        if (rect != null)
+            rect.localRotation = Quaternion.Euler(0f, 180f, 0f);
     }
 }
