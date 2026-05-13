e9dd2ff5f171cbb5f7f04c40f23214ab669ff7a9
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-13 22:29:01 +0900

    Update Breakable_Obj.cs

--- stage1/Breakable_Obj.cs
+++ stage1/Breakable_Obj.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 public class Breakable_Obj : MonoBehaviour
 {
-    [SerializeField] private GameObject dropPrefab;
-
-    private SoundEffect_Manager soundEffect;
-
-    void Start()
-    {
-        soundEffect = FindObjectOfType<SoundEffect_Manager>();
-    }
+    [SerializeField]
+    GameObject Drop_prefap;
+    private SpriteRenderer Sprite_;
+    public SoundEffect_Manager soundEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Bullet")) return;
-
-        if (soundEffect != null)
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
             soundEffect.Effect_Sound("ITEMBREAK");
+            GameObject drop_item = Instantiate(Drop_prefap, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
 
-        if (dropPrefab != null)
-            Instantiate(dropPrefab, transform.position, transform.rotation);
+    
 
-        Destroy(gameObject);
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sprite_ = GetComponent<SpriteRenderer>();
+        soundEffect = GameObject.FindObjectOfType<SoundEffect_Manager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
