b79d7ac0274306b200ed8f486aa77f5e8b8682f4
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-13 18:27:35 +0900

    Update CameraController.cs

--- boss/CameraController.cs
+++ boss/CameraController.cs
@@ -4,63 +4,79 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject target; 
+
+    public GameObject target; // 카메라가 따라갈 대상
     public GameObject BossTarget;
 
     public Vector3 bossTransform;
 
-    public float moveSpeed = 3;
-    private Vector3 targetPosition; 
+    public float moveSpeed = 3;// 카메라가 따라갈 속도
+    private Vector3 targetPosition; // 대상의 현재 위치
+    //public GameObject character;
 
     public Boss_Trigger is_boss;
 
+    // Start is called before the first frame update
     void Start()
     {
         is_boss = GameObject.FindObjectOfType<Boss_Trigger>();
 
         BossTarget = GameObject.Find("Boss");
         target = GameObject.Find("Character");
-        
         if (BossTarget != null)
         {
-            bossTransform = new Vector3(
-                BossTarget.transform.position.x, -1f, transform.position.z);
+            //bossTransform = BossTarget.transform;
+            bossTransform.Set(BossTarget.transform.position.x, -1, this.transform.position.z);
         }
+        
     }
 
     public void Character_movement()
     {
-      if(target == null) return;
+        // this는 카메라를 의미 (z값은 카메라값을 그대로 유지)
+        targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
 
-      targetPosition = new Vector3(target.transform.position.x, target.transform.y, transform.position.z);
-
-      transform.position =  Vector3.Lerp(transform.position, targetPosition, movwSpeed * Time.deltaTime);        
+        // vectorA -> B까지 T의 속도로 이동
+        this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        
     }
 
     public IEnumerator Fixed_Boss()
     {
-        while (Vector3.Distance(transform.position, bossTransform) > 0.01f)
+        do
         {
-            transform.position = Vector3.Lerp(
-                transform.position,
-                bossTransform,
-                moveSpeed * Time.deltaTime
-            );
+            this.transform.position = Vector3.Lerp(this.transform.position, bossTransform, moveSpeed * Time.deltaTime);
+            yield return new WaitForSeconds(0.01f);
 
-            yield return null;
-        }
+        } while (this.transform.position != bossTransform);
 
-        transform.position = bossTransform;
+        this.transform.position=bossTransform;
     }
 
+
+    // Update is called once per frame
     public void Update()
     {
-       if (target == null) return;
-
-        if (is_boss != null && is_boss.Is_bossStage)
-            return;
-
-        Character_movement();
+        
+        // 대상이 있는지 체크
+        if (target.gameObject != null)
+        {
+            if (is_boss != null)
+            {
+                if (is_boss.Is_bossStage == false)
+                {
+                    Character_movement();
+                }
+                
+            }
+            else if (is_boss == null)
+            {
+                Character_movement();
+            }
+            
+            
+        }
+        
 
     }
 }
