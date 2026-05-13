bb09f8bb5f208b79a7682fcc54f4fd8338aeae75
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-13 18:22:47 +0900

    Update Loading_Text.cs

--- LodingScene/Loading_Text.cs
+++ LodingScene/Loading_Text.cs
@@ -6,31 +6,43 @@ using TMPro;
 
 public class Loading_Text : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI dialog_context;
+    public TextMeshProUGUI dialog_context;
 
+    // Start is called before the first frame update
     void Start()
     {
-        if(dialog_context == null)
-        {
-            dialog_context = GetComponentInChildren<TextMeshProUGUI>();
-        }
-        dialog_context.text = "";
-        StartCoroutine(Typing("Now Loading...")); 
+        dialog_context = this.GetComponentInChildren<TextMeshProUGUI>();
+        dialog_context.text = null;
+        StartCoroutine(Typing("Now Loading..."));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 
     IEnumerator Typing(string dialogue)
     {
-      while (true)
+        //이 함수가 시작할 때 한 번 내용물을 싹 비워준다(안그러면 전의 대사에 추가해서 나옴)
+        //위의 Typing_trigger에서 dialogue를 가져와서 talk라는 지역변수?로 사용
+        do
         {
-            dialog_context.text = "";
+            //dialog_context.text = null;
+
 
             for (int i = 0; i < dialogue.Length; i++)
             {
-                dialog_context.text += dialogue[i];
-                yield return new WaitForSeconds(0.15f);
+                dialog_context.text = null;
+                string talk = dialogue;
+                for (int j = 0; j < talk.Length; j++)
+                {
+                    dialog_context.text += talk[j];
+                    yield return new WaitForSeconds(0.2f); //코러틴 함수에서 사용하는 함수로, 지정된 시간에 한번씩 돌아가게 됨 > 글자 출력 속도랑 같다고 볼 수 있음
+                }
+                yield return new WaitForSeconds(0.3f);
             }
+        } while (true);
 
-            yield return new WaitForSeconds(1f);
-        }
     }
 }
