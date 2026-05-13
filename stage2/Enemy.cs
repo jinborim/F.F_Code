895a56e327c8590dd524cee781526c6b0b272278
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-13 23:01:43 +0900

    Update Enemy.cs

--- stage2/Enemy.cs
+++ stage2/Enemy.cs
@@ -1,42 +1,49 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-  [SerializeField] private float speed;
+    public float speed;
 
-    private bool isMovingLeft = true;
+    bool isLeft = true;
 
+    // Start is called before the first frame update
     void Start()
     {
-        speed = Random.Range(2f, 6f);
+        speed = Random.Range(2, 6);
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (Inventory.invectoryActivated) return;
+        if (Inventory.invectoryActivated == false)
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
 
-        Move();
     }
-
-    private void Move()
+    void TurnAround()
     {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
-    }
 
-    private void TurnAround()
-    {
-        isMovingLeft = !isMovingLeft;
-
-        float yRotation = isMovingLeft ? 0f : 180f;
-        transform.eulerAngles = new Vector3(0f, yRotation, 0f);
+        if (isLeft) // 왼쪽으로 간다면
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0); // Y축 방향을 180도로 반대로 돌려 오른쪽으로 가게함
+            isLeft = false;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0); // 오른쪽으로 가고 있다가 충돌시 다시 왼쪽으로 몸을 돌려 걸어감
+            isLeft = true;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!collision.gameObject.CompareTag("endpoint")) return;
-
-        TurnAround();
+        // 특정 endpoint와 충돌시 방향 전환
+        if (collision.gameObject.CompareTag("endpoint"))
+        {
+            TurnAround();
+        }
     }
 }
