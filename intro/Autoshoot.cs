f1fe7512119ab6caac5fd240682b8fdebfdb24b3
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-13 18:31:23 +0900

    Update Autoshoot.cs

--- intro/Autoshoot.cs
+++ intro/Autoshoot.cs
@@ -5,35 +5,40 @@ using UnityEngine;
 public class Autoshoot : MonoBehaviour
 {
     private Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    //public enemyDead enemy;
+    public GameManager gamemanager;
 
-    [SerializeField] private GameObject bulletPrefab;
-    private GameManager gamemanager;
 
+    // Start is called before the first frame update
     void Start()
     {
-        bulletSpawnPoint = transform.Find("BulletSpawnPoint");
-
-        gamemanager = FindObjectOfType<GameManager>();
-
+        bulletSpawnPoint = this.transform.Find("BulletSpawnPoint");
+        gamemanager = GameObject.FindObjectOfType<GameManager>();
+        //enemy = GameObject.FindObjectOfType<enemyDead>();
         StartCoroutine(AutoBullet());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 
     public IEnumerator AutoBullet()
     {
-        if (bulletPrefab == null) yield break;
-
-        for (int i = 0; i < 3; i++)
+        for(int i = 0; i<3; i++)
         {
-            Vector3 spawnPos = bulletSpawnPoint != null 
-                ? bulletSpawnPoint.position 
-                : transform.position;
+            //ÃÑ¾ËÀ» ¸¸µç´Ù 
+            /* var bulletGo = Instantiate<GameObject>(this.bulletPrefab);
+            bulletGo.transform.position = this.bulletSpawnPoint.position; */
+            GameObject BulletGo = Instantiate(bulletPrefab, this.transform.position, transform.rotation);
 
-            Instantiate(bulletPrefab, spawnPos, transform.rotation);
-
-            yield return new WaitForSeconds(0.6f);
+            yield return new WaitForSeconds(0.6f);// + Á¶°Ç
         }
+        gamemanager.Scene22(true);
+        yield break;
 
-        if (gamemanager != null)
-            gamemanager.Scene22(true);
+
     }
 }
