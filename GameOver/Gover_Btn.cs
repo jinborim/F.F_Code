dbd642f7b913e09639a069aa249b2464c65ee1b6
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-13 18:20:14 +0900

    Update Gover_Btn.cs

--- GameOver/Gover_Btn.cs
+++ GameOver/Gover_Btn.cs
@@ -6,13 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class Gover_Btn : MonoBehaviour, IPointerClickHandler
 {
-    [SerializeField] private STAGE_Maintain stage;
+    public STAGE_Maintain stage;
 
     public void OnPointerClick(PointerEventData eventData)
     {
         if (eventData.button == PointerEventData.InputButton.Left)
         {
+            //SceneManager.LoadScene(stage.sceneNow);
             LoadSceneManager.LoadScene(stage.sceneNow);
         }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stage = GameObject.FindObjectOfType<STAGE_Maintain>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
