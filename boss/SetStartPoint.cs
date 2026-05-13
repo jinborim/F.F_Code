4c705e4052443dde9edf6150ab180e6cc85a75ce
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-13 18:29:13 +0900

    Update SetStartPoint.cs

--- boss/SetStartPoint.cs
+++ boss/SetStartPoint.cs
@@ -3,34 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 public class SetStartPoint : MonoBehaviour
 {
+    //public string startPoint;
     private GameObject thePlayer;
-    
     public float Xpoint;
     public float Ypoint;
     public Vector2 startpoint;
 
+    // Start is called before the first frame update
     void Start()
     {
-       startpoint = new Vector2(Xpoint, Ypoint);
-
+        startpoint = new Vector2(Xpoint, Ypoint);
+        // 플레이어 참조가 없을 경우 자동으로 찾아 할당
         if (thePlayer == null)
-            thePlayer = GameObject.Find("Character");
-
-        if (thePlayer != null)
-            thePlayer.transform.position = startpoint;
-
-        var dialogMaintain = Dialogue_Maintain.Instance;
-
-        if (dialogMaintain != null)
         {
-            Dialog_Test dialog = dialogMaintain.GetComponentInChildren<Dialog_Test>();
-            Select_base select = dialogMaintain.GetComponentInChildren<Select_base>();
-
-            if (dialog != null)
-                dialog.gameObject.SetActive(false);
-
-            if (select != null)
-                select.gameObject.SetActive(false);
+            thePlayer = GameObject.Find("Character");
         }
+        
+        // 플레이어를 해당 씬의 시작 지점으로 즉시 이동
+        thePlayer.transform.position = startpoint;
+
+
+        // 씬 전환 시 남아있는 대화창 및 선택지 UI를 가에 비활성화
+        // 중복 UI출력을 방지하여 화면 가독성과 안전성 설계
+        if(Dialogue_Maintain.Instance.GetComponentInChildren<Dialog_Test>()&& Dialogue_Maintain.Instance.GetComponentInChildren<Select_base>()!= null)
+        {
+            Dialogue_Maintain.Instance.GetComponentInChildren<Dialog_Test>().gameObject.SetActive(false);
+            Dialogue_Maintain.Instance.GetComponentInChildren<Select_base>().gameObject.SetActive(false);
+        }
+        
+
     }
 }
