812bc0b8167b8a2a1a64f6e67531ebe47391615a
Jin Borim <121596050+jinborim@users.noreply.github.com>
2026-05-13 22:55:14 +0900

    Update Next_Door.cs

--- stage1/Next_Door.cs
+++ stage1/Next_Door.cs
@@ -4,47 +4,41 @@ using UnityEngine;
 
 public class Next_Door : MonoBehaviour
 {
-    public string SceneName; 
-
+    public string SceneName;
     private Door_Test door;
     public Select_Yes selected;
-
-    public bool is_Enter = false; 
-    public string[] Door_dialogue = new string[]{"다음 스테이지로 이동할까?"};
-
-
-   void Start()
-    {
-        door = GameObject.FindObjectOfType<Door_Test>();
-
-        if (door != null)
-        {
-            selected = door.select_.GetComponentInChildren<Select_Yes>();
-        }
-    }
+    public bool is_Enter = false;
+    public string[] Door_dialogue = new string[2];
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Character"))
-            return;
-
-        is_Enter = true;
-
-        if (selected != null)
+        if (collision.gameObject.CompareTag("Character") == true)
+        {
+            
+            //Debug.Log("캐릭터랑 닿음");
+            is_Enter = true;
             selected.GetSceneName(SceneName);
-
-        if (door != null)
             door.Get_Dialogue(is_Enter, Door_dialogue, true);
+            
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Character"))
-            return;
+        if (collision.gameObject.CompareTag("Character") == true)
+        {
+            //Debug.Log("충돌 해제");
+            is_Enter = false;
+            door.Get_Dialogue(is_Enter, null, false);
 
-        is_Enter = false;
+        }
+    }
 
-        if (door != null)
-            door.Get_Dialogue(false, null, false);
+    void Start()
+    {
+        door = GameObject.FindObjectOfType<Door_Test>();
+        selected = door.select_.GetComponentInChildren<Select_Yes>();
+        Door_dialogue =new string[] { "다음 스테이지로 이동할까?" };
+        
     }
 }
